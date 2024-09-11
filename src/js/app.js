437bafaba95fbf9aaf565bc2a5234aaef8114c5b
Joaquin Morales <Mintinvestments95@gmail.com>
2024-09-11 00:19:31 +0000

    almost finished

--- src/js/app.js
+++ src/js/app.js
@@ -34,9 +34,9 @@ function render(variables = {}) {
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1> ${variables.name}</h1>
-          <h2>${variables.role} || </h2>
+          <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
-          <ul class="position-right">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
